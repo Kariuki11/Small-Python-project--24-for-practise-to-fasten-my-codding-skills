cfa6893bafb835f8f86b65c89965aac581ac9bdb
Kariuki11 <irungukenneth433@gmail.com>
2024-07-04 20:54:18 +0300

    new commits

--- project1.py
+++ project1.py
@@ -49,4 +49,6 @@ while max(player_scores) < max_score:
             player_scores[player_idx] += current_score
             print("Your total score is:", player_scores[player_idx])
             
-            
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("player number", winning_idx + 1, "is the winner with a score of:", max_score)
