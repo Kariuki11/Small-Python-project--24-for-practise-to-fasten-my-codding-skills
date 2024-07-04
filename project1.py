acfb63a9698b34a1f5142dca57dc9e59602ff2f2
Kariuki11 <irungukenneth433@gmail.com>
2024-07-04 20:38:11 +0300

    New changes

--- project1.py
+++ project1.py
@@ -28,17 +28,22 @@ while max(player_scores) < max_score:
     for player_idx in range(players):
         current_score = 0
         
-        should_roll = input("Would you like to roll (y)? ")
-        if should_roll.lower() != "y":
-            break
-        
-        value = roll()
-        if value == 1:
-            print("You rolled a 1! Turn down!.")
-            break
-        else:
-            current_score += value
-            print("You rolled a", value)
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
             
-            print("Your Score is:", current_score)
-        
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn down!.")
+                break
+            else:
+                current_score += value
+                print("You rolled a", value)
+                
+                print("Your Score is:", current_score)
+                
+            player_scores[player_idx] += current_score
+            print("Your total score is:", player_scores[player_idx])
+            
+            
