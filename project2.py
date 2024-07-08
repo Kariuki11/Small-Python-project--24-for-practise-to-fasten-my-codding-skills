8a6269b4220ec2ece229d8513a5acf90f29987f3
Kariuki11 <irungukenneth433@gmail.com>
2024-07-08 20:44:44 +0300

    New commits

--- project2.py
+++ project2.py
@@ -1,2 +1,12 @@
 with open("story.txt", "r") as f:
     story = f.read()
+    
+words = []
+start_of_word = -1
+
+target_start = "<"
+target_end = ">"
+    
+for i, char in enumerate(story):
+    if char == 
+    
